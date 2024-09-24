e4bfe1bb370aad9d03de53ee21d6266b4807f160
rferrall1738 <rferrall@calpoly.edu>
2024-09-24 11:07:07 -0700

    Update database.py

--- src/database.py
+++ src/database.py
@@ -10,11 +10,11 @@ def database_connection_url():
 engine = create_engine(database_connection_url(), pool_pre_ping=True)
 
 with engine.begin() as connection:
-    connection.execute(sqlalchemy.text(" INSERT INTO global_inventory (num_green_potions, num_green_ml, gold) VALUES ('0', '0', '100' ))
-
     result = connection.execute(sqlalchemy.text("SELECT * FROM global_inventory"))
+    
     for row in result:
         print(row)
+        
     connection.execute(sqlalchemy.text("UPDATE global_inventory SET num_green_potions = '300'))
     
 
