667b625f2d07dc26e221c60b65966711a37720bf
rferrall1738 <rferrall@calpoly.edu>
2024-10-04 08:08:16 -0700

    no leak

--- src/database.py
+++ src/database.py
@@ -4,8 +4,7 @@ from sqlalchemy import create_engine
 
 def database_connection_url():
     dotenv.load_dotenv()
-    POSTGRES_URI= "postgresql://postgres.fdzisynlybwfvbtspgak:pOTIONpAPI720@aws-0-us-west-1.pooler.supabase.com:6543/postgres"
-    return os.environ.get(POSTGRES_URI)
+    return os.environ.get("POSTGRES_URI")
 
 engine = create_engine(database_connection_url(), pool_pre_ping=True)
 
